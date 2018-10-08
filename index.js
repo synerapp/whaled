4dc5cc7b0bc49e3868d3c43ddf0ea73515683db4
ederaleng <ederandreslobo1998@gmail.com>
2018-10-08 17:13:30 -0500

    add timer

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-//dependencias
 const express = require("express")
 const app = express()
 const morgan = require('morgan')
 const path = require("path")
 const favicon = require('serve-favicon');
 const wls = require("wlsjs");
-wls.api.setOptions({ url: 'http://whaleshares.io/ws' });
+const moment = require('moment');
+var current=0
+
+moment().format("YYYYMMDDhhmmss"); 
+nodo()
+setInterval(()=>{ nodo() },10*60*1000)
+function nodo(){
+    const noneCurrent=[
+        'http://188.166.99.136:8090',
+        'http://whaleshares.io/ws',
+        'http://beta.whaleshares.net/wss'
+    ]
+    wls.api.setOptions({ url: noneCurrent[current] });
+    console.log(`current node ${noneCurrent[current]}`)
+    current>=noneCurrent.length-1 ? current=0 : current++
+    console.log(`next node ${noneCurrent[current]}`)
+}
 
 
 app.set('view engine', 'ejs');
@@ -34,29 +49,43 @@ var data=[]
 var last_trans
 var datosU=[]
 var manejoerrores=false
+var fech=[]
 function cargarhistorial(usuario,start,callback){
     wls.api.getAccounts([usuario], (err, response)=>{
         if(err){
+            nodo()
             manejoerrores=true
             if(callback)
                 callback(err)
         }
-        if(response)
+        if(response){
             datosU.push(response[0])
+        }
+
     })
-    wls.api.getAccountHistory(usuario, start , 100, function(err, result) {
+    wls.api.getAccountHistory(usuario, start , (start < 0) ? 10000 : Math.min(start, 10000), function(err, result) {
         if(err){
+            nodo()
             manejoerrores=true
             if(callback)
                 callback(err)
         }
         if(result){
-            result.reverse();
-            for(var i = 0; i < result.length; i++) {
-                data.push(result[i]);
+        result.reverse();
+        for(var i = 0; i < result.length; i++) {
+            var trans = result[i];
+            data.push(result[i]);
+            var fpreviud=moment.utc(result[i][1].timestamp).valueOf()
+            fech.push(moment(fpreviud).fromNow())
+            // Save the ID of the last transaction that was processed.
+            last_trans=trans[0];
+        }
+        if(last_trans > 0 && last_trans != start && datosU[0])
+            cargarhistorial(usuario, last_trans, callback);
+            else {
+                if(callback)
+                    callback(data,null,datosU)
             }
-            if(callback)
-                callback(data,null,datosU)
         }else{
             callback(null,err,null)
         }
@@ -65,6 +94,7 @@ function cargarhistorial(usuario,start,callback){
 function confis(callback){
     wls.api.getDynamicGlobalProperties((err, result) => {
         if(err){
+            nodo()
             manejoerrores=true
             if(callback)
                 callback(err)
@@ -80,30 +110,47 @@ function confis(callback){
     });
 }
 
+app.get('/',()=>{
+    res.status(200).send('index')
+})
+
+
 app.get('/:id',(req,res)=>{
     sp=null
     data=[]
     user=null
     datosU=[]
+    fech=[]
     var user=req.path,
         page=req.query.page
     if(user!="/@"){
         user.toLowerCase();
-        cargarhistorial(user.substr(2,user.length),(page ? (page*-100) : -100),(data,err,datau)=>{
-            if(err){
-                res.status(200).render("errores")
-            }else{
+        cargarhistorial(user.substr(2,user.length),-1,(data,err,datau)=>{
+            if(err)
+                res.render("errores")
+            else{
                 confis((gsp)=>{
+                    if(manejoerrores){
+                        res.status(200).render("errores")
+                    }
                     if(gsp){
+                        var nPages=parseInt(data.length)/100,
+                            pagef= page ? page*100 : 0,
+                            pagef2=(parseInt(pagef)+101)>data.length ? data.length : parseInt(pagef)+101,
+                            pagefinal= pagef && pagef2 ? data.slice(pagef,pagef2) : data.slice(0,101)
+                            
+                        var fin=(parseInt(pagef)+101)>fech.length ? fech.length : parseInt(pagef)+101,
+                                ffinal= pagef && fin ? fech.slice(pagef,fin) : fech.slice(0,101)
+
                         res.status(200).render('usernames',{
-                            datos:data,
+                            page:page,
+                            datos:pagefinal,
+                            numPages:nPages,
+                            fech:ffinal,
                             u:user.substr(2,user.length),
                             datau:datau,
-                            page:page,
                             sp:gsp
                         })
-                    }else{
-                        res.status(200).render("errores")
                     }
                 })
             }
@@ -115,6 +162,7 @@ app.get('/:id',(req,res)=>{
 function buscarinfo(trxid,callback){
     wls.api.getTransaction(trxid, function(err, result) {
         if(err){
+            nodo()
             manejoerrores=true
             if(callback)
                 callback(err)
@@ -133,11 +181,9 @@ app.get('/trx/:id',(req,res)=>{
             res.status(200).render("errores")
         }
         //res.send(datossend)
-        else{
-            res.status(200).render('trx',{
-                data:datossend
-            })
-        }
+        res.status(200).render('trx',{
+            data:datossend
+        })
     })
 })
 
